708a2314c013ef6953b53e3bb36b46342b2edaf7
Adrian Setniewski <274433@pw.edu.pl>
2018-03-15 12:38:40 +0100

    Added products view and Product class.

---
+++ coder-swag/View/ProductCell.swift
@@ -0,0 +1,15 @@
+//
+//  ProductCell.swift
+//  coder-swag
+//
+//  Created by Adrian Setniewski on 15.03.2018.
+//  Copyright © 2018 Adrian Setniewski. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+}
